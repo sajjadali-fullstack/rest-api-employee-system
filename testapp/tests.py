2e3d424dd802d2f80504cade5b9854d4e8c579ee
Sajjad Ali <sajjadali.dev01@gamil.com>
2026-05-17 21:22:07 +0530

    Completed Employee CRUD REST API using DRF

--- testapp/tests.py
+++ testapp/tests.py
@@ -21,24 +21,51 @@ END_POINT = 'api/'
 
 
 
-def create_resource():
-    new_emp = {
+# def create_resource():
+#     new_emp = {
 
-        'emp_id':3,
-        'first_name':'Aleem',
-        'last_name':'Sayyed',
-        'email':'aleem@gmail.com' ,
-        'department':'Accountant',
-        'designation':'Jr. Accountant',
-        'salary':45000,
+#         'emp_id':3,
+#         'first_name':'Aleem',
+#         'last_name':'Sayyed',
+#         'email':'aleem@gmail.com' ,
+#         'department':'Accountant',
+#         'designation':'Jr. Accountant',
+#         'salary':45000,
+#         'joining_date':'2026-05-17',
+#         'City':'Thane',
+#         'Address':'Mumbra',
+
+#     }
+#     # conver to JSON
+#     resp = requests.post(BASE_URL + END_POINT, data=json.dumps(new_emp))
+#     print(resp.status_code)
+#     print(resp.json)
+
+# create_resource()
+
+
+def update_resource(id):    
+    update_emp = {
+        'id':id,
+        'emp_id':4,
+        'first_name':'Lambu',
+        'last_name':'Bhaiya',
+        'email':'lambu@gmail.com',
+        'department':'JR',
+        'designation':'Blinkit',
+        'salary':15000,
         'joining_date':'2026-05-17',
-        'City':'Thane',
-        'Address':'Mumbra',
+        'City':'Mumbra',
+        'Address':'MM',
+
 
     }
-    # conver to JSON
-    resp = requests.post(BASE_URL + END_POINT, data=json.dumps(new_emp))
-    print(resp.status_code)
-    print(resp.json)
+    resp = requests.put(BASE_URL + END_POINT, data=json.dumps(update_emp))
 
-create_resource()
+    print('='*30)
+    print(resp.status_code)
+    print(resp.json())
+    print('='*30)
+
+update_resource(3)
+
